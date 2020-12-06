c46b334a2737c2d662521a66fcfd7a2d087fd2f1
Martin Weissbach <martin.weissbach@scandio.de>
2020-12-06 14:39:33 +0100

    day 6 part one

---
+++ day_6/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("day_6/input.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer file.Close()
+
+	var answerCount int
+	groupAnswers := make(map[uint8]bool)
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" || line == "\n" {
+			answerCount += len(groupAnswers)
+			groupAnswers = make(map[uint8]bool)
+			continue
+		}
+		for i := 0; i < len(line); i++ {
+			groupAnswers[line[i]] = true
+		}
+	}
+	answerCount += len(groupAnswers)
+
+	if err := scanner.Err(); err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Printf("Sum of Answers: %d\n", answerCount)
+}
