d363a752c17448a17473ea49656f81120b99a6b7
Martin Weissbach <martin.weissbach@scandio.de>
2020-12-22 09:33:06 +0100

    day 15 part one

---
+++ day_15/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "log"
+
+func main() {
+	log.Printf("Part One - 2020th number: %d\n", partOne([]int{0, 1, 5, 10, 3, 12, 19}, 2020))
+}
---
+++ day_15/part_1.go
@@ -0,0 +1,22 @@
+package main
+
+func partOne(startingNumbers []int, terminationRound int) int {
+	brain := make(map[int][]int)
+	for i, number := range startingNumbers {
+		brain[number] = append(brain[number], i+1)
+	}
+
+	lastNumber := startingNumbers[len(startingNumbers)-1]
+	for round := len(startingNumbers) + 1; round <= terminationRound; round++ {
+		prev := brain[lastNumber]
+		if len(prev) == 1 {
+			brain[0] = append(brain[0], round)
+			lastNumber = 0
+			continue
+		}
+		lastNumber = prev[len(prev)-1] - prev[len(prev)-2]
+		brain[lastNumber] = append(brain[lastNumber], round)
+	}
+
+	return lastNumber
+}
